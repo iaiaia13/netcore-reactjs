e8b9d2f0ae21d9b56afd24acf1ff85edda0d52db
ducthinh121994 <ducthinh121994@gmail.com>
2020-05-20 00:54:49 +0800

    Initial project

---
+++ Server/API/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext context)
+        {
+            this._context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var result = await _context.Value.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+            var result = await _context.Value.FindAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetForecasts")]
+        public IEnumerable<WeatherForecast> GetForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
